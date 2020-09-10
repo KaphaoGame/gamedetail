109d0b5841980c6f1620da6a0d1361d6d9f20351
phantichchai <phantichchai1998@gmail.com>
2020-09-11 03:33:56 +0700

    add getmapping

--- src/main/java/com/thirajade/gamedetai/api/GameDetailController.java
+++ src/main/java/com/thirajade/gamedetai/api/GameDetailController.java
@@ -1,6 +1,8 @@
 package com.thirajade.gamedetai.api;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +17,9 @@ public class GameDetailController {
 	public GameRequest getAllGameRequest(){
 	    return gameRequest;
 	}
-	
+
+	@GetMapping(value = "/gameDetail")
+	public ResponseEntity index() {
+		return ResponseEntity.ok(gameRequest);
+	}
 }
