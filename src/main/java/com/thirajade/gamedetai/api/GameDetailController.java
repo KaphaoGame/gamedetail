68006498591db8789aceab10dc7eaa79c3a78d0a
phantichchai <phantichchai1998@gmail.com>
2020-09-12 16:13:33 +0700

    heroku initial

--- src/main/java/com/thirajade/gamedetai/api/GameDetailController.java
+++ src/main/java/com/thirajade/gamedetai/api/GameDetailController.java
@@ -3,23 +3,23 @@ package com.thirajade.gamedetai.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
 public class GameDetailController {
 
-	GameRequest gameRequest = new GameRequest("valorant");
+	private List<GameRequest> myGameRequest = new ArrayList();
 
-	@RequestMapping("/gameDetail")
-	public GameRequest getAllGameRequest(){
-		return gameRequest;
+	public GameDetailController() {
+		myGameRequest.add(new GameRequest("valorant"));
 	}
 
-	@GetMapping(value = "/gameDetail")
+	@GetMapping(value = "/")
 	public ResponseEntity index() {
-		return ResponseEntity.ok(gameRequest);
+		return ResponseEntity.ok(myGameRequest);
 	}
 }
