e6d6f8cffdd02684c4b71d63ccbfe9d36079b4d2
phantichchai <phantichchai1998@gmail.com>
2020-11-02 02:43:41 +0700

    new list in function

--- src/main/java/com/thirajade/gamedetai/api/GameDetailController.java
+++ src/main/java/com/thirajade/gamedetai/api/GameDetailController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 public class GameDetailController {
 
-	private List<GameRequest> myGameRequest = new ArrayList();
+	private List<GameRequest> myGameRequest;
 
 	public GameDetailController() {
 
@@ -23,18 +23,14 @@ public class GameDetailController {
 
 	@GetMapping(value = "/")
 	public ResponseEntity checkToGameRequestList(@RequestParam(value="name") String name) {
-		if (myGameRequest.size() > 0) {
-			myGameRequest.remove(myGameRequest.get(0));
-		}
+		myGameRequest = new ArrayList<>();
 		myGameRequest.add(new GameRequest(name));
 		return ResponseEntity.ok(myGameRequest);
 	}
 
 	@PostMapping(value = "/")
 	public ResponseEntity addToGameRequestList(@RequestParam(value="name") String name) {
-		if (myGameRequest.size() > 0) {
-			myGameRequest.remove(myGameRequest.get(0));
-		}
+		myGameRequest = new ArrayList<>();
 		myGameRequest.add(new GameRequest(name));
 		return ResponseEntity.ok(myGameRequest);
 	}
